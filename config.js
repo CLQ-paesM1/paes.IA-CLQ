4de5963b98bce53ac76c272f73c6af9cb0003163
CLQ-paesM1 <pelayoberrios@hotmail.com>
2025-07-31 22:25:41 -0400

    Update config.js

--- config.js
+++ config.js
@@ -7,4 +7,4 @@
  * tutor.html a través de la variable global OPENAI_API_KEY.
  */
 
-const OPENAI_API_KEY = '';
+const OPENAI_API_KEY = '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';
